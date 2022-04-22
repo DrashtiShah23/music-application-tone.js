2783c9a7775fe6360f5cc18836c4390e37f1b86e
Bsandhu97 <78628964+Bsandhu97@users.noreply.github.com>
2022-04-22 00:18:46 -0700

    Updated State file

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -2,13 +2,17 @@
 import { List, Map } from 'immutable';
 
 // project dependencies
+//Instruments
 import { PianoInstrument } from './instruments/Piano';
 import {GuitarInstrument} from './instruments/WilliamZ';
+import { HarpInstrument  } from './instruments/Bsandhu97';
+import { ClarinetInstrument} from './instruments/swethac1130'
+
+//Visualizer
 import { WaveformVisualizer } from './visualizers/Waveform';
 import { FlowerVisualizer } from './visualizers/WilliamZ';
-import { ClarinetInstrument} from './instruments/swethac1130'
 import { OvalVisualizer  } from './visualizers/swethac1130';
-
+import { MosaicVisualizer  } from './visualizers/Bsandhu97';
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
  ** ------------------------------------------------------------------------ */
@@ -18,13 +22,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument,GuitarInstrument, ClarinetInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, HarpInstrument, GuitarInstrument, ClarinetInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer, FlowerVisualizer, OvalVisualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, FlowerVisualizer, OvalVisualizer, MosaicVisualizer ]);    // similar to Visualizer[]
 
 
 /**
