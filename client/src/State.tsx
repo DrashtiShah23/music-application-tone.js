fc6c2fdb4fd09551cd31b0a5b2d581c3d2f77dfc
wzhong1 <wzhong@mail.sfsu.edu>
2022-04-26 16:07:49 -0700

    final project update

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -10,6 +10,8 @@ import { FlowerVisualizer } from './visualizers/WilliamZ';
 import { ClarinetInstrument} from './instruments/swethac1130'
 import { OvalVisualizer  } from './visualizers/swethac1130';
 import {DrashtiShah23Visualizer} from './visualizers/DrashtiShah23'
+import { HarpInstrument } from './instruments/Bsandhu97';
+import {MosaicVisualizer} from './visualizers/Bsandhu97'
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
  ** ------------------------------------------------------------------------ */
@@ -19,13 +21,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument,GuitarInstrument, ClarinetInstrument, TromboneInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument,GuitarInstrument, ClarinetInstrument, TromboneInstrument, HarpInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer, FlowerVisualizer, OvalVisualizer, DrashtiShah23Visualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, FlowerVisualizer, OvalVisualizer, DrashtiShah23Visualizer, MosaicVisualizer]);    // similar to Visualizer[]
 
 
 /**
--- server/src/sql/get_songs.sql
+++ server/src/sql/get_songs.sql
@@ -1,1 +1,1 @@
-SELECT * FROM songs;
+SELECT * FROM songs WHERE song_title LIKE '%?%';
